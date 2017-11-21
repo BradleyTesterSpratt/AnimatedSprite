d2c4412a9eeb708fdb0905ea8d60dd426cee328e
Bradley Tester (student) <bst19@uni.brighton.ac.uk>
2017-11-21 10:52:06 +0000

    Initial commit

---
+++ android/src/com/testersprattb/animatedsprite/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.testersprattb.animatedsprite;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.testersprattb.animatedsprite.AnimatedSprite;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnimatedSprite(), config);
+	}
+}
---
+++ desktop/src/com/testersprattb/animatedsprite/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.testersprattb.animatedsprite.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.testersprattb.animatedsprite.AnimatedSprite;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnimatedSprite(), config);
+	}
+}
